9f17b99e67ed1ee3e72a1ebffd6b394bbf625a18
--replace-all <ignaciobellomo@gmail.com>
2019-11-12 15:00:58 -0500

    border radius and card lined up

---
+++ starter-code/src/components/id-card-component/id-card.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class IDCard extends React.Component {
+
+
+
+    createCard = () => {
+        return (
+            <div className="id-card">
+                
+            </div>
+
+
+
+        )
+    }
+
+
+
+    render(){
+
+
+
+        return (
+
+
+        )
+    }
+
+
+}
+
+export default IDCard;
